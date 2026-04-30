7e56710f216e6212b00de02ff7f660af3e6e9258
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-30 19:16:34 +0700

    Mencari posisi insert

--- DoubleLinkListed.cpp
+++ DoubleLinkListed.cpp
@@ -58,4 +58,12 @@ public:
             return;
         }
 
-        
+        // insert in between node
+        // Step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+       
