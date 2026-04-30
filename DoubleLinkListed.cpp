0c3c0b90d50b32f859bc4ddb3289609a80aaba3e
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-30 18:53:14 +0700

    Alokasi node baru

--- DoubleLinkListed.cpp
+++ DoubleLinkListed.cpp
@@ -28,4 +28,10 @@ public:
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
 
+       // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data fields
+        newNode->noMhs = nim;
+
         
